90618dfeea4c4b397e222e43ad1fe821502dffe4
JnAcero <juanacbu128@gmail.com>
2023-09-17 02:07:52 -0500

    creacion de test unitarios

---
+++ src/Exercises/Challenges.cs
@@ -0,0 +1,66 @@
+﻿namespace Exercises;
+
+public class Challenges
+{
+    public static string Panic(string sentence)
+    {
+        if (sentence.Contains(" "))
+        {
+            sentence = sentence.ToUpper().Replace(" ", " X ") + "!";
+        }
+        else
+        {
+            sentence = sentence.ToUpper() + "!";
+        }
+        return sentence;
+    }
+    public static string Whisper(string sentence)
+    {
+        sentence = $"shh... {sentence.ToLower()}";
+        if (sentence.EndsWith("!"))
+        {
+            sentence = sentence.Replace("!", "");
+        }
+        return sentence;
+    }
+    public static string AltCaps(string sentence)
+    {
+        string newString = "";
+        char[] caracteres = sentence.ToCharArray();
+        for (int i = 0; i < caracteres.Length; i++)
+        {
+            if (i % 2 == 0) newString += caracteres[i].ToString().ToUpper();
+            else newString += caracteres[i];
+        }
+        return newString;
+    }
+    public static string EmojifyWord(string word)
+    {
+        Dictionary<string, string> emojis = new()
+        {
+            {"smile","😁"},
+            {"angry","😡"},
+            {"party","🥳"},
+            {"heart","♥"},
+            {"cat","🐱"},
+            {"dog","🐶"},
+        };
+        string copyLower = word.ToLower();
+        if (copyLower.StartsWith(":") && copyLower.EndsWith(":")){
+            copyLower = copyLower.Replace(":", "");
+            if (emojis.ContainsKey(copyLower)) return emojis[copyLower];
+            else return copyLower;
+        }
+        else return word;
+    }
+    public static string EmojifyPhrase(string phrase)
+    {
+        var words = phrase.Split(" ").Select(x =>EmojifyWord(x)).ToList();
+        string phraseEmojified = string.Join(" ",words);
+        Console.WriteLine(phraseEmojified);
+
+        return phraseEmojified;
+    }
+
+
+}
---
+++ test/ExerciseTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+using Exercises;
+
+namespace ExerciseTest;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod_Panic()
+    {
+        string sentence = "The winter is coming";
+        string resultado = Challenges.Panic(sentence);
+        Assert.AreEqual("THE X WINTER X IS X COMING!", resultado);
+    }
+    [TestMethod]
+    public void TestMethod_Whisper()
+    {
+        string[] sentences = { Challenges.Whisper("PLEASE STOP SHOUTING."), Challenges.Whisper("MA'M, this is a library!") };
+
+        Assert.AreEqual("shh... please stop shouting.", sentences[0]);
+        Assert.AreEqual("shh... ma'm, this is a library", sentences[1]);
+    }
+    [TestMethod]
+    public void TestMethod_AltCaps()
+    {
+        string sentence = "I'm so happy it's Monday";
+        string result = Challenges.AltCaps(sentence);
+
+        Assert.AreEqual("I'M So hApPy iT'S MoNdAy", result);
+    }
+    [TestMethod]
+    public void TestMethod_EmojifyWordWithProperFormat()
+    {
+        string catWord = ":cat:";
+        string dogWord = ":dog:";
+        string elephantWord = "elephant";
+
+        string resultCat = Challenges.EmojifyWord(catWord);
+        string resultDog = Challenges.EmojifyWord(dogWord);
+        string resultElephant = Challenges.EmojifyWord(elephantWord);
+
+        Assert.AreEqual("🐱", resultCat);
+        Assert.AreEqual("🐶", resultDog);
+        Assert.AreEqual("elephant", resultElephant);
+    }
+    [TestMethod]
+    public void TestMethod_EmojifyPhraseWithProperFormat()
+    {
+        string test1 = "I :heart: my cat";
+        string test2 = "I :heart: the :party: with my :boyfrind:";
+
+        string result1 = Challenges.EmojifyPhrase(test1);
+        string result2 = Challenges.EmojifyPhrase(test2);
+
+        Assert.AreEqual("I ♥ my cat",result1);
+        Assert.AreEqual("I ♥ the 🥳 with my boyfrind",result2);
+    }
+
+
+
+}
